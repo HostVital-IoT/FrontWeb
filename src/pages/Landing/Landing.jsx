ffebe0efb1d1e0100556cdd374edc67870a833bb
Saf-ire <carlosacostadominguez@gmail.com>
2021-08-02 11:36:35 -0600

    JSX

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../../components/header/header.jsx'
+import Footer from '../../components/footer/footer.jsx'
+import './Landing.css'
+
+const Landing = (props) => {
+    return (
+        <React.Fragment>
+            <Header></Header>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>Document</title>
+                </head>
+                <body>
+                    <div className="Landing">
+
+                    </div>
+                </body>
+            </html>
+            <Footer></Footer>
+        </React.Fragment>
+    )
+}
+
+export default Landing
