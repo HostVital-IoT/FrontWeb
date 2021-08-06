4eb7915e343ef2953d4643b05ac94c6552e29d2b
Cesar Erives <cesarerives43@gmail.com>
2021-08-06 00:29:14 -0600

    Path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from './pages/Landing/Landing.jsx';
 import LogIn from './pages/Login/Login.jsx';
 import SignUp from './pages/Signup/Signup.jsx';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NewPatient from './pages/NewPatient/NewPatient.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" component={Landing}></Route>
         <Route exact path="/login" component={LogIn}></Route>
         <Route exact path="/signup" component={SignUp}></Route>
+        <Route exact path="/newpatient" component={NewPatient}></Route>
       </Switch>
     </BrowserRouter>
   );
