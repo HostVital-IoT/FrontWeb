3c1e95db5f9646e7bd1561e63ab6537a29120d59
Cesar Erives <cesarerives43@gmail.com>
2021-08-07 00:12:12 -0600

    added patient page needs finishing

---
+++ src/pages/patient/patient.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/header'
+import Footer from '../../components/footer/footer'
+import './patient.css'
+
+const Patient = (props)=> {
+    return(
+        <React.Fragment>
+            <Header></Header>
+                
+            <Footer></Footer>
+        </React.Fragment>
+    )
+}
