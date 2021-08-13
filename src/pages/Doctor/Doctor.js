ddb87b124d98918f6112987a8b4b8d792691f80b
Saf-ire <carlosacostadominguez@gmail.com>
2021-08-13 13:01:23 -0600

    fixed

---
+++ src/pages/Doctor/Doctor.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from '../../components/header/header2'
+import Footer from '../../components/footer/footer'
+import { Link } from 'react-router-dom'
+import './Doctor.css'
+
+const Doctor = (props) => {
+    return (
+        <React.Fragment>
+            <Header></Header>
+            <div className="doctorContainer">
+                <div className="doctorimg">
+                    <img src="https://img.gruporeforma.com/imagenes/960x640/6/63/5062370.jpg" />
+                </div>
+                <div className="doctorname">
+                    <h3>Emilio Soto</h3>
+                </div>
+            </div>
+
+
+            <Link to="/patients">
+                <button className="dr__Button">Patients</button>
+            </Link>
+            <Footer></Footer>
+        </React.Fragment>
+    )
+}
+
+export default Doctor;
